34e36293c419d9ef7b7d6b02386dd2c6549badc7
Sami Fink <fink.s.m.8651@gmail.com>
2024-04-09 20:45:06 +0000

    created database and inserted data in teams table

--- insert_data.sh
+++ insert_data.sh
@@ -8,3 +8,50 @@ else
 fi
 
 # Do not change code above this line. Use the PSQL variable above to query your database.
+
+# clearing data
+echo $($PSQL "TRUNCATE TABLE games, teams")
+
+# reading through the rows
+cat games.csv | while IFS="," read YEAR ROUND WINNER OPPONENT WINNER_GOALS OPPONENT_GOALS
+do
+  # as long as were are not in the title row
+  if [[ $YEAR != "year" ]]
+  then
+    # get team id of winners
+    TEAM_ID=$($PSQL "SELECT team_id FROM teams WHERE name='$WINNER'")
+
+    # if not found
+    if [[ -z $TEAM_ID ]]
+    then
+      # insert team
+      INSERT_TEAM_RESULT=$($PSQL "INSERT INTO teams(name) VALUES('$WINNER')")
+
+      # creating message
+      if [[ $INSERT_TEAM_RESULT == "INSERT 0 1" ]]
+      then
+        echo Inserted team: $WINNER
+      fi
+
+    fi  
+
+    # same for opponents
+    TEAM_ID=$($PSQL "SELECT team_id FROM teams WHERE name='$OPPONENT'")
+
+    # if not found
+    if [[ -z $TEAM_ID ]]
+    then
+      # insert team
+      INSERT_TEAM_RESULT=$($PSQL "INSERT INTO teams(name) VALUES('$OPPONENT')")
+
+      # creating message
+      if [[ $INSERT_TEAM_RESULT == "INSERT 0 1" ]]
+      then
+        echo Inserted team: $OPPONENT
+      fi
+
+    fi 
+
+   fi 
+
+done
